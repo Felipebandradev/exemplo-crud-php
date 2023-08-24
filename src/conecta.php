884ec48611028b4880ba2660c0410a8ccd9e449f
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-24 09:44:04 -0300

    Script de conexão com PDO

---
+++ src/conecta.php
@@ -0,0 +1,35 @@
+<?php
+// conecta.php
+
+/* Script de conexão ao servidor de Banco de dados */
+
+// Parâmetros
+// Xampp
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas";
+
+/* Configurações para conexão (API/Driver de conexão: PDO (PHP Data Object))*/
+try{
+
+    // Instância/Objeto para conexão
+    $conexao = new PDO(
+        "mysql:host=$servidor;dbname=$banco;cherset=utf8",
+        $usuario,$senha
+    );
+
+    // Habilitar a Verificação e sinalização de erros(exeções)
+    $conexao->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch(Exception $erro){
+    /* Exception é uma classe/tipo 
+    voltado para tratamento de exceções (erros)*/
+    die("Deu ruim: ".$erro->getMessage());
+}
+
+
+
