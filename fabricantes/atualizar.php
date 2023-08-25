d90995508b963ff46c85c8a6c8c81a3b56e4e784
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-25 11:22:16 -0300

    Carregamento dos dados Fabricante

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -1,9 +1,12 @@
 <?php
+require_once "../src/funcoes-fabricantes.php";
 /* Obtendo de sanitizando o valor vindo da url  (link dinâmico) */
 $id = filter_input(INPUT_GET,"id", FILTER_SANITIZE_NUMBER_INT);
 
+$fabricante = ler_um_fabricante($conexao, $id);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -39,6 +42,7 @@ p a{
 form{
     width: 500px;
     margin: auto;
+    
 }
 
 input[type=text] {
@@ -73,9 +77,10 @@ button {
     <hr>
 
     <form action="" method="post">
+        <input type="hidden" name="id" value="<?=$fabricante["id"]?>">
         <p>
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome" required>
+            <input value="<?=$fabricante["nome"]?>" type="text" name="nome" id="nome" required> 
         </p>
 
         <button type="submit" name="atualizar">atualizar Fabricante</button>
